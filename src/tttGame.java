ac8289712f283994302c55f61d8b9a8259daf750
24michaell2 <80217349+24michaell2@users.noreply.github.com>
2021-10-16 12:38:14 -0700

    Add files via upload

--- src/tttGame.java
+++ src/tttGame.java
@@ -32,6 +32,7 @@ public class tttGame extends JPanel {
                         playerMark = 'x';
                         buttonClicked.setBackground(Color.ORANGE);
                     }
+                    displayVictor();
                 }
 
             });
@@ -43,17 +44,20 @@ public class tttGame extends JPanel {
 
     public void displayVictor() {
 
-        if (checkForWinner() == true) {
+        if (checkForWinner()) {
+            System.out.println("winner found");
             if (playerMark == 'x') playerMark = 'o';
             else playerMark = 'x';
 
             JOptionPane pane = new JOptionPane();
             int dialogResult = JOptionPane.showConfirmDialog(pane, "Game Over. " + playerMark + " wins. Would you like to play again?", "Game over.", JOptionPane.YES_NO_OPTION);
 
-            if (dialogResult == JOptionPane.YES_OPTION) resetTheButtons();
-            else System.exit(0);
+            if (dialogResult == JOptionPane.YES_OPTION) {
+                resetTheButtons();
+            } else {
+                System.exit(0);
+            }
         } else if (checkDraw()) {
-
             JOptionPane pane = new JOptionPane();
             int dialogResult = JOptionPane.showConfirmDialog(pane, "Draw. Play again?", "Game over.", JOptionPane.YES_NO_OPTION);
             if (dialogResult == JOptionPane.YES_OPTION) resetTheButtons();
@@ -70,7 +74,7 @@ public class tttGame extends JPanel {
     }
 
     public boolean checkDraw() {
-        for (int i = 0; i < 9; i++) {
+        for (int i = 0; i <= 8; i++) {
             if (buttons[i].getText().charAt(0) == '-') {
                 return false;
             }
@@ -79,5 +83,40 @@ public class tttGame extends JPanel {
     }
 
     public boolean checkForWinner() {
+        // COLUMNS
+        //for (int j = 0; j < 3; j++) {
+        for (int i = 0; i < 3; i++) {
+            if (buttons[i].getText().equals(buttons[i + 3].getText()) && buttons[i].getText().equals(buttons[i + 6].getText()) && buttons[i].getText().charAt(0) != '-') {
+                return true;
+            }
+        }
+        //}
+
+        // ROWS
+        for (int i = 0; i < 9; i += 3) {
+            if (buttons[i].getText().equals(buttons[i + 1].getText()) && buttons[i].getText().equals(buttons[i + 2].getText()) && buttons[i].getText().charAt(0) != '-') {
+                return true;
+            }
+        }
+
+        if (buttons[0].getText().equals(buttons[4].getText()) && buttons[0].getText().equals(buttons[8].getText()) && buttons[0].getText().charAt(0) != '-') {
+            return true;
+        }
+
+        if (buttons[2].getText().equals(buttons[4].getText()) && buttons[2].getText().equals(buttons[6].getText()) && buttons[2].getText().charAt(0) != '-') {
+            return true;
+        }
+
+        return false;
+    }
+
+
+    public static void main(String[] args) {
+        JFrame window = new JFrame("Tic Tac Toe");
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.getContentPane().add(new tttGame());
+        window.setBounds(500, 500, 500, 500);
+        window.setVisible(true);
+        window.setLocationRelativeTo(null);
     }
 }
